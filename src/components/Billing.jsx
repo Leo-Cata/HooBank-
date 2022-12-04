eeff8d8b40db67e0374c818852cd9f647822b616
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2022-12-04 18:49:23 -0300

    1:28:36

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
-const Billing = () => {
-  return <div>Billing</div>;
-};
+const Billing = () => (
+  <section id='product' className={layout.sectionImgReverse}>
+    <div className=''></div>
+  </section>
+);
 
 export default Billing;
